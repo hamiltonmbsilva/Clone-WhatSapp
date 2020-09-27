207f1fffc89b39db4933e13be683c7a69f83bfdf
Hamilton <hamiltonmb.silva@gmail.com>
2020-09-27 19:49:13 -0300

    Criação do layout do whatsapp do header do lado siderbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export default () => {
+  return(
+    <div className="app-window">
+      <div className="sidebar">
+          <header>
+            <img className="header--avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRsRX6QzCX2rxKzfmn7VlsMYA0TYTW5Nt6o5g&usqp=CAU" alt="Avatar" />            
+            <div className="header-bottons">
+              <div className="header--btn">
+                <DonutLargeIcon style={{color:'#919191'}} />
+              </div>
+              <div className="header--btn">
+                <ChatIcon style={{color:'#919191'}} />
+              </div>
+              <div className="header--btn">
+                <MoreVertIcon style={{color:'#919191'}} />
+              </div>            
+            </div>
+          </header>
+
+          <div className="search">
+            ...
+          </div>
+
+          <div className="chatlist">
+            ...
+          </div>
+      </div>
+
+      <div className="contentarea">
+        ...
+      </div>
     </div>
   );
 }
-
-export default App;
