c7f0d303614458cec895af077911b0d5b4b15cd5
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-17 18:15:41 -0300

    configuração do firebase e e criação do bando de dados no firebase, e conectado firebase ao projeto whatappclone

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyD3lt-0rfgsQJShddnBZ-kzz0Vum0hG_l0",
+    authDomain: "whatsappclone-ee0e5.firebaseapp.com",
+    databaseURL: "https://whatsappclone-ee0e5.firebaseio.com",
+    projectId: "whatsappclone-ee0e5",
+    storageBucket: "whatsappclone-ee0e5.appspot.com",
+    messagingSenderId: "79807856469",
+    appId: "1:79807856469:web:65102392098db32acf79dc"
+  };
